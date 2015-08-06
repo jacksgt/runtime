362d5f265af3179c176c2067b1c9ab93c2b25ac8
Jack Henschel <jackhenschel97@gmail.com>
2015-08-06 14:26:08 +0200

    Fixed values in global.h (RUNTIME, RUNTIME_CACHE and CACHE_INTERVAL)

--- globals.h
+++ globals.h
@@ -5,9 +5,9 @@
 
 #define MAX_LINE_LENGTH 64 /* /proc/uptime can't be read with fseek, there a static value is required */
 #define UPTIME "/proc/uptime" /* path to uptime provided by the kernel */
-#define RUNTIME "go/var/runtime" /* path where runtime should store its value */
-#define RUNTIME_CACHE "go/var/runtime_cache" /* path where runtime should store the cache */
-#define CACHE_INTERVAL 30 /* interval at which runtime should cache its value (in seconds) */
-#define VERSION "0.2" /* current version of Runtime */
+#define RUNTIME "/var/cache/runtime" /* path where runtime should store its value */
+#define RUNTIME_CACHE "/var/cache/runtime_cache" /* path where runtime should store the cache */
+#define CACHE_INTERVAL 3600 /* interval at which runtime should cache its value (in seconds) */
+#define VERSION "0.3" /* current version of Runtime */
 
 #endif
